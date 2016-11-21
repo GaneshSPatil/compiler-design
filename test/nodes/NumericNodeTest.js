7b420f300e410e17736da7414f968ab865b6a69d
GaneshSPatil <ganeshpl@thoughtworks.com>
2016-11-21 18:48:16 +0530

    Added test for NumericNode

---
+++ test/nodes/NumericNodeTest.js
@@ -0,0 +1,34 @@
+var NumericNode = require('../../lib/nodes/NumericNode.js');
+var expect = require('chai').expect;
+
+describe('NumericNode', function(){
+  it('should have value field containing numeric value', function(){
+    var two = new NumericNode(2);
+    expect(two.value).to.equal(2);
+  });
+
+  it('should not have any arguments', function(){
+    var two = new NumericNode(2);
+    expect(two.args).to.eql([]);
+  });
+
+  it.skip('type should be number', function(){
+    var two = new NumericNode(2);
+    expect(two.type).to.equal('Number');
+  });
+
+  it('should evaluate itself and return the value', function(){
+    var two = new NumericNode(2);
+    expect(two.evaluate({})).to.equal(2);
+  });
+
+  it('should have string representation of the value', function(){
+    var two = new NumericNode(2);
+    expect(two.represent()).to.equal('two');
+  });
+
+  it('should have string representation of big number value', function(){
+    var num = new NumericNode(1000000);
+    expect(num.represent()).to.equal('one million');
+  });
+});
