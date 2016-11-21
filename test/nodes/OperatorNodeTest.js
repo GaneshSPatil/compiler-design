2fc5a14775a9eb8891def622d14687dfe2021aaa
GaneshSPatil <ganeshpl@thoughtworks.com>
2016-11-21 20:54:41 +0530

    Added tests for OperatorNode

---
+++ test/nodes/OperatorNodeTest.js
@@ -0,0 +1,80 @@
+var OperatorNode = require('../../lib/nodes/OperatorNode.js');
+var NumberNode = require('../../lib/nodes/NumericNode.js');
+var VariableNode = require('../../lib/nodes/VariableNode.js');
+var AssignmentNode = require('../../lib/nodes/AssignmentNode.js');
+
+var expect = require('chai').expect;
+
+describe('OperatorNode', function(){
+  it('should have plus operator as value', function(){
+    var expr = new OperatorNode('+', [new NumberNode(1), new NumberNode(2)]);
+    expect(expr.value).to.equal('+');
+  });
+
+  it('should have minus operator as value', function(){
+    var expr = new OperatorNode('-', [new NumberNode(1), new NumberNode(2)]);
+    expect(expr.value).to.equal('-');
+  });
+
+  it('should have multiplication operator as value', function(){
+    var expr = new OperatorNode('*', [new NumberNode(1), new NumberNode(2)]);
+    expect(expr.value).to.equal('*');
+  });
+
+  it('should have division operator as value', function(){
+    var expr = new OperatorNode('/', [new NumberNode(1), new NumberNode(2)]);
+    expect(expr.value).to.equal('/');
+  });
+
+  it('should have Operation as type', function(){
+    var expr = new OperatorNode('+', [new NumberNode(1), new NumberNode(2)]);
+    expect(expr.type).to.equal('Operation');
+  });
+
+  it('should evaluate the addition expression', function(){
+    var expr = new OperatorNode('+', [new NumberNode(1), new NumberNode(2)]);
+    expect(expr.evaluate({}).evaluate()).to.eql(3);
+  });
+
+  it('should evaluate the subtraction expression', function(){
+    var expr = new OperatorNode('-', [new NumberNode(1), new NumberNode(2)]);
+    expect(expr.evaluate({}).evaluate()).to.eql(-1);
+  });
+
+  it('should evaluate the multiplication expression', function(){
+    var expr = new OperatorNode('*', [new NumberNode(1), new NumberNode(2)]);
+    expect(expr.evaluate({}).evaluate()).to.eql(2);
+  });
+
+  it('should evaluate the division expression', function(){
+    var expr = new OperatorNode('/', [new NumberNode(1), new NumberNode(2)]);
+    expect(expr.evaluate({}).evaluate()).to.eql(0.5);
+  });
+
+  it('should look up for a variable in variable pool', function(){
+    var expr = new OperatorNode('+', ['a', new NumberNode(2)]);
+    expect(expr.evaluate({'a' : new NumberNode(1)}).evaluate()).to.equal(3);
+  });
+
+  it('should throw error when the variable does not exist in variable pool', function(){
+    var expr = new OperatorNode('+', ['a', new NumberNode(2)]);
+    expect(function(){expr.evaluate({});}).throw('a is not defined');
+  });
+
+  it('should evaluate complex expressions', function(){
+    var addExpr = new OperatorNode('+', [new NumberNode(2), new NumberNode(2)]);
+    var expr = new OperatorNode('/', [addExpr, new NumberNode(2)]);
+    expect(expr.evaluate({}).evaluate()).to.eql(2);
+  });
+
+  it('should represent simple expressions', function(){
+    var expr = new OperatorNode('*', [new NumberNode(1), new NumberNode(2)]);
+    expect(expr.represent()).to.eql(['*', 'one', 'two']);
+  });
+
+  it('should represent complex assignment expressions', function(){
+    var addExpr = new OperatorNode('+', [new NumberNode(2), new NumberNode(3)]);
+    var expr = new OperatorNode('*', [new NumberNode('2'), addExpr]);
+    expect(expr.represent()).to.eql(['*', 'two', ['+', 'two', 'three']]);
+  });
+});
