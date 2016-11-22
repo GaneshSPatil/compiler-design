e50dfcdf1757d7f5c3eb194346517faa2f171d00
GaneshSPatil <ganeshpl@thoughtworks.com>
2016-11-22 12:01:07 +0530

    Seperated JS walker from generic walker

--- foo.js
+++ foo.js
@@ -1,4 +1,5 @@
 var treesWalker = require('./treeWalker.js');
+var jsTreesWalker = require('./JSTreeWalker.js');
 
 var Parser = require("jison").Parser;
 var fs = require('fs');
@@ -8,11 +9,21 @@ var grammar = fs.readFileSync('./exprEvaluator.jison', 'utf8');
 var parser = new Parser(grammar);
 var trees = parser.parse(process.argv[2]);
 
-// var result = treesWalker.walk(trees, 'represent');
-// console.log(result);
+console.log('--------------------------------');
+var result = treesWalker.walk(trees, 'represent');
+console.log('------ REPRESENT TREE ----------');
+console.log(result);
+console.log('--------------------------------');
+var result = treesWalker.walk(trees, 'evaluate');
+console.log('------- EVALUATE TREE ----------');
+console.log(result[result.length - 1].evaluate());
 
-// var result = treesWalker.walk(trees, 'evaluate');
-// console.log(result[result.length - 1].evaluate());
+console.log('--------------------------------');
 
-var result = treesWalker.walk(trees, 'toJS');
+var result = jsTreesWalker.walk(trees, 'toJS');
+console.log('------- JAVASCRIPT CODE --------');
 console.log(result.join('\n'));
+console.log('--------------------------------');
+console.log('------- JS CODE RESULT ---------');
+eval(result.join('\n'));
+console.log('--------------------------------');
--- treeWalker.js
+++ treeWalker.js
@@ -2,13 +2,6 @@ var walk = function(trees, key){
   var variables = {};
   var result = trees.reduce(function(resultHolder, tree){
     var value = tree[key](variables);
-    if(tree.type == 'Factorial'){
-      var factFn = 'var factorial = function(num){ if(num == 1){ return num;} return num*factorial(num-1);};'
-      resultHolder.unshift(factFn);
-    }
-    if(tree.type != 'Assignment'){
-      value = 'console.log('+value+');'
-    }
     resultHolder.push(value);
     return resultHolder;
   }, []);
